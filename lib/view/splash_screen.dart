233c29208d1ce1bc691310e0c0b381f2c0ef10f0
Hamamul Fauzi <hamamul.fauzi@gmail.com>
2022-04-02 14:24:52 +0700

    add empty splash screen

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class SplashScren extends StatefulWidget {
+  const SplashScren({ Key? key }) : super(key: key);
+
+  @override
+  State<SplashScren> createState() => _SplashScrenState();
+}
+
+class _SplashScrenState extends State<SplashScren> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
